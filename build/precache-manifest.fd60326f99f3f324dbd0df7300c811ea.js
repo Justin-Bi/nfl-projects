135fb1edeba5a80e2d5c032faada5bac68d3d90d
Justin Bi <justinbi247@gmail.com>
2020-08-17 18:43:12 -0400

    more

---
+++ build/precache-manifest.fd60326f99f3f324dbd0df7300c811ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0018dcfe58afd76e1b3e9fcc43ad44b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a8fed26f2a6dd2cc1ea",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e5bb7a434703358490e",
+    "url": "/static/js/2.da69bb92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da69bb92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a8fed26f2a6dd2cc1ea",
+    "url": "/static/js/main.a106e7ff.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
