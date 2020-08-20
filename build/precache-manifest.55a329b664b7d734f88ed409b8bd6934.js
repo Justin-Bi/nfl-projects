1076bc8b73b96c1d32a74af3ff9cdad37f1f89ba
Justin Bi <justinbi247@gmail.com>
2020-08-19 23:42:45 -0400

    Testing api call

---
+++ build/precache-manifest.55a329b664b7d734f88ed409b8bd6934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ec528d39b8a4058d4d65db8495ec87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "380afa0ed56613e64fa6",
+    "url": "/static/css/main.a284afd3.chunk.css"
+  },
+  {
+    "revision": "44e0d259de9b07b7347d",
+    "url": "/static/js/2.cd351080.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd351080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380afa0ed56613e64fa6",
+    "url": "/static/js/main.97b27e95.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  }
+]);
