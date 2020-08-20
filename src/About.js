daf24aefc975842fce162b1a02fcf51f245b085a
Justin Bi <justinbi247@gmail.com>
2020-08-19 23:40:05 -0400

    Test api call

--- src/About.js
+++ src/About.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function About() {
+  const [currentTime, setCurrentTime] = useStatete(0);
+  
+  useEffectct(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div>
       <h1>About Page</h1>
+      <p>The current time is {currentTime}</p>
     </div>
   );
 }
