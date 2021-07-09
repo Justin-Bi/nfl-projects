245635bfa22d6078d130064c759aadc01d35dba1
Justin Bi <justinbi247@gmail.com>
2021-07-08 23:09:42 -0400

    Added the build

---
+++ build/precache-manifest.49c59e7b00c10a0f2096fed4880485fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d671ce4b09103df8e5a168e795f9e512",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b8ec39126dd3e7954a",
+    "url": "/static/css/main.a284afd3.chunk.css"
+  },
+  {
+    "revision": "a8637c00f9be8d7fc9cb",
+    "url": "/static/js/2.4e319123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e319123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b8ec39126dd3e7954a",
+    "url": "/static/js/main.17e38992.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  }
+]);
