37b7815e17cc3c12c8f5631f0447cf50602554f2
Justin Bi <justinbi247@gmail.com>
2021-07-08 23:25:51 -0400

    Debug: Try unpickling the graph

--- api/api.py
+++ api/api.py
@@ -1,7 +1,14 @@
 import time
-from flask import Flask
+from flask import Flask, request
+import pickle
+from Graph import Graph
+from Vertex import Vertex
+import json
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
+
+with open('./nfl_graph.pkl', 'rb') as f:
+    g = pickle.load(f)
 
 @app.route('/')
 def index():
